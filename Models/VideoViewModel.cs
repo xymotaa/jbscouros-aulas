8cd3142d56ae92154e953956336972939ba6ef98
Lucas Mota <lucas.barros@unifesspa.com>
2026-03-11 20:11:40 -0300

    Initial commit

---
+++ Models/VideoViewModel.cs
@@ -0,0 +1,12 @@
+namespace jbscouros_aulas.Models
+{
+    public class VideoViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Duration { get; set; }
+        public string ThumbnailUrl { get; set; }
+        public string Category { get; set; }
+        public string PublishDate { get; set; }
+    }
+}
